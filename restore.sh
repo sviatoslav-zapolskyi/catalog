3aaaefc6c5cafa43486682a165abda387f120309
Sviatoslav Zapolskyi <sviatoslav.zapolskyi@opower.com>
2019-04-26 16:42:18 +0300

    restor script

---
+++ restore.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "" ] ; then
+    echo "provide backup folder name"
+else
+    BACKUP_HOME=$1
+
+    TAG=$(cat ${BACKUP_HOME}/git_tag.txt)
+
+    git checkout ${TAG}
+
+    echo "stop containers"
+    docker stop $(docker ps -a -q)
+
+    echo  "remove all containers"
+    docker rm $(docker ps -a -q)
+
+    rm tmp/pids/server.pid
+
+    docker-compose up --build --detach
+
+    read -p 'provide mysql container id: ' MYSQL_CONTAINER
+
+    rm -rf ./storage/
+    tar -zxvf ${BACKUP_HOME}/storage.tar.gz
+
+    cat ${BACKUP_HOME}/mysqldump.sql | docker exec -i ${MYSQL_CONTAINER} /usr/bin/mysql -u root -ppassword catalog_development
+fi
