c468baa3b0424f572f627e00049f472eac5c826c
Sviatoslav Zapolskyi <sviatoslav.zapolskyi@opower.com>
2022-11-12 18:58:34 +0200

    Firefox 80: Expected "handle" to be a string, got [object Undefined] undefined

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 TAG=$(git describe --tags)
 BACKUP_HOME=backups/${TAG}_$(date +"%s")
 
-mkdir -p ${BACKUP_HOME}
-echo ${TAG} > ${BACKUP_HOME}/git_tag.txt
-tar -zcvf ${BACKUP_HOME}/storage.tar.gz ./storage/
-docker exec catalog_db_1 /usr/bin/mysqldump -u root -ppassword catalog_development > ${BACKUP_HOME}/mysqldump.sql
+mkdir -p "${BACKUP_HOME}"
+echo "${TAG}" > "${BACKUP_HOME}/git_tag.txt"
+tar -zcvf "${BACKUP_HOME}/storage.tar.gz" ./storage/
+docker exec catalog_db_1 /usr/bin/mysqldump -u root -ppassword catalog_development > "${BACKUP_HOME}/mysqldump.sql"
