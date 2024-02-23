25df9f6b2097f7710ab4590592151a9542879e23
Sviatoslav Zapolskyi <sviatoslav.zapolskyi@gmail.com>
2024-02-23 18:33:21 +0200

    Backup/Restor scripts improvements

--- backup.sh
+++ backup.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+source check_pv_installed.sh
+
 TAG=$(git describe --tags)
 BACKUP_HOME=backups/${TAG}_$(date +"%s")
 
 mkdir -p "${BACKUP_HOME}"
 echo "${TAG}" > "${BACKUP_HOME}/git_tag.txt"
-tar -zcvf "${BACKUP_HOME}/storage.tar.gz" ./storage/
+#tar -zcvf "${BACKUP_HOME}/storage.tar.gz" ./storage/
+tar cf - ./storage/ -P | pv -s $(($(du -sk ./storage/ | awk '{print $1}') * 1024)) | gzip > "${BACKUP_HOME}/storage.tar.gz"
+
 docker-compose exec db /usr/bin/mysqldump -u root -ppassword catalog_development > "${BACKUP_HOME}/mysqldump.sql"
--- restore.sh
+++ restore.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source check_pv_installed.sh
+
 if [ "$1" == "" ] ; then
     echo 'provide backup folder name'
 else
@@ -16,14 +18,14 @@ else
     docker-compose down --timeout 1
 
     rm -rf ./storage/
-    tar -zxvf ${BACKUP_HOME}/storage.tar.gz
+    pv ${BACKUP_HOME}/storage.tar.gz | tar --extract --gzip
 
     rm tmp/pids/server.pid
 
     docker-compose up --build --detach
 
     echo 'Restore mysqldump.sql'
-    while ! cat ${BACKUP_HOME}/mysqldump.sql | docker-compose exec db /usr/bin/mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} catalog_development >/dev/null 2>&1 ; do
+    while ! cat ${BACKUP_HOME}/mysqldump.sql | docker-compose exec --no-TTY db /usr/bin/mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} catalog_development >/dev/null 2>&1 ; do
         echo 'Wait mysql to start ...'
         sleep 3
     done
